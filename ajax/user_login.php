65acdf24dc0b02b48945c1ab9b0f49c664c28c8c
Kebab <martin@matejka.xyz>
2021-10-10 14:09:58 +0200

    login ajax

---
+++ ajax/user_login.php
@@ -0,0 +1,56 @@
+<?php
+session_start();
+
+require "../defines.php";
+
+require ROOT."/classes/database.class.php";
+
+if(isset($_POST['email']) && isset($_POST['heslo'])){
+
+	$db = new Database();
+	if($db->error) return "Nedá sa pripojiť k DB";
+	$conn = $db->handle;
+
+	$stmt = $conn->prepare("SELECT * FROM User WHERE email = ? LIMIT 1");
+	$stmt->bind_param("s",$_POST["email"]);
+ 	$stmt->execute();
+ 	$res = $stmt->get_result();
+
+ 	if($res->num_rows < 1){
+ 		echo json_encode(
+			array(
+				"success" => false,
+				"error" => "Zadaný email nie je registrovaný"
+			)
+		);
+		return;
+ 	}
+ 	$data = $res->fetch_assoc();
+
+ 	if(password_verify($_POST["heslo"], $data["password"])){
+ 		//heslo je ok
+
+ 		$_SESSION['user'] =array(
+ 			"email" => $_POST["email"],
+ 			 "role" => $data['role']
+ 		) ;
+
+ 		echo json_encode(
+			array(
+				"success" => true,
+				"error" => ""
+			)
+		);
+		return;	
+ 	}
+ 	else{
+ 		echo json_encode(
+			array(
+				"success" => false,
+				"error" => "Nesprávne heslo"
+			)
+		);
+		return;
+ 	}
+
+}
