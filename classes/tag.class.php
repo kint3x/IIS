14d1acd5a7a834462988660224a96f9c913dc7d9
Simon Košina <simonkosina@gmail.com>
2021-11-12 19:22:11 +0100

    Showing tags

---
+++ classes/tag.class.php
@@ -0,0 +1,69 @@
+<?php
+
+include_once "../defines.php";
+
+class Tag {
+
+    public static $error_message = "";
+
+    /**
+     * Create a new tag in the Tag table with the given name.
+     */
+    public static function create_tag($name) {
+        $db = new Database();
+
+		if ($db->error) {
+			self::$error_message = 'Problém s pripojením k databáze.';
+			return false;
+		}
+
+		$conn = $db->handle;
+		
+		$stmt = $conn->prepare('INSERT INTO Tag (name) VALUES (?)');
+		$stmt->bind_param('s', $name);
+
+		if (!$stmt->execute()) {
+			self::$error_message = 'Chyba pri načítaní údajov.';
+			$db->close();
+			return false;
+		};
+		
+		$db->close();
+
+		return true;
+    }
+
+    // public static function add_tag_to_conference($conferrence_id, $tag_id) {
+		
+    // }
+
+    public static function get_conference_tags($conferrence_id) {
+		$db = new Database();
+		
+		if ($db->error) {
+			self::$error_message = 'Problém s pripojením k databáze.';
+			return false;
+		}
+
+		$conn = $db->handle;
+		
+		$stmt = $conn->prepare(
+			'SELECT * FROM Tag WHERE id IN (SELECT genre_id FROM `cross_conf_tag` WHERE conference_id = ?)'
+		);
+		$stmt->bind_param('i', $conferrence_id);
+
+		if (!$stmt->execute()) {
+			self::$error_message = 'Chyba pri načítaní údajov.';
+			$db->close();
+			return false;
+		};
+		
+		$res = $stmt->get_result();
+		$tags = $res->fetch_all(MYSQLI_ASSOC);
+		
+		$db->close();
+
+		return $tags;
+    }
+
+}
