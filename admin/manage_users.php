9353e2732150cb7311b5dd30b98945c04c4739cb
Martin Matějka <martin@matejka.xyz>
2021-11-14 19:31:49 +0100

    prerobenie na simpletable

--- admin/manage_users.php
+++ admin/manage_users.php
@@ -23,176 +23,58 @@
    </div>
    <div class="row">
       <div class="col-md-12">
-         <div class="table-responsive">
-            <table id="mytable" class="table table-bordred table-striped">
-               <thead>
-                  <th><input type="checkbox" id="checkall" /></th>
-                  <th>ID</th>
-                  <th>Meno</th>
-                  <th>Priezvisko</th>
-                  <th>Adresa</th>
-                  <th>Email</th>
-                  <th>Rola</th>
-                  <th>Edit</th>
-                  <th>Delete</th>
-               </thead>
-               <tbody>
-               	<?php 
-               	if(!isset($_GET['page'])){
-               		$curr_page = 0;
-               	}
-               	else{
-               		$curr_page = $_GET['page']-1;
-               	}
+      	<?php 
+      	$table = new SimpleTable("User",
+				    	array(
+				    		"table_id" => "users", //unikatne id tabulky na stranke
+				    		"ajax_url" => "/ajax/admin_user_action.php", // ajax pre spracovanie poziadavkov
+				    		"delete" => true,
+				    		"edit" => true,
+				    		"add" => true
+						)
+				    );
 
-               	$cnt_res = User::get_all_users_count();
+      			//premenovanie stlpca v hlavicke
 
-               	// 10 = per page
-               	$last_page = intval($cnt_res/10)+1;
-               	$users = User::get_all_users(10,$curr_page*10);
+				    $table->table_structure['id']['name'] = "ID";
+				    $table->table_structure['surname']['name'] = "Priezvisko";
+				    $table->table_structure['name']['name'] = "Meno";
+				    $table->table_structure['role']['name'] = "Rola";
+				    $table->table_structure['email']['name'] = "E-mail";
+				    $table->table_structure['address']['name'] = "Adresa";
+				    $table->table_structure['password']['name'] = "Heslo";
 
-               	foreach( $users as $row){
-               		echo "<tr id='row".$row['id']."'>";
-               		echo '<td><input type="checkbox" value="'.$row['id'].'" class="checkthis"/></td>';  
-               		echo "<td>".$row['id']."</td>";
-               		echo "<td>".$row['name']."</td>";
-               		echo "<td>".$row['surname']."</td>";
-               		echo "<td>".$row['address']."</td>";
-               		echo "<td>".$row['email']."</td>";
-               		$role = ($row['role']) ? "Admin" : "Užívateľ";
-               		echo "<td>".$role."</td>";
-               		echo '<td><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#editUserModal" >Upraviť</span></button></td>';
-               		echo '<td><button class="btn btn-danger btn-xs" onclick="delete_user('.$row['id'].')" >Vymazať</button></td>';
-               		echo "</tr>";
-               	}
+				     // nahradit role 1 a 0 s vyznamom
+				    $table->table_structure['role']['override'] = array(
+				    	USER_ADMIN => "Admin",
+				    	USER_REGULAR => "User"
+				    );
 
-               	?>
-                  
-               </tbody>
-            </table>
-            <div class="clearfix"></div>
-            <button class="btn btn-danger btn-xs" onclick="delete_checked()" style="margin-bottom: 15px;float:right;" >Vymazať označené</button>
-            <?php
-            echo getPaginationString($curr_page+1,$last_page);
-             ?>
-         </div>
+				    //schovat zobrazovanie password column
+				    $table->table_structure['password']['show_column'] = false;
+
+				    //nechcem aby ho vo formulari prefillovalo
+				    $table->table_structure['password']['form_edit_prefill'] = false;
+
+
+
+				  
+				    
+
+
+
+				    echo $table->generate_table_html();
+
+      	?>
       </div>
    </div>
 </div>
-		<!-- Modal na upravu užívateľov -->
-		<div class="modal fade" id="editUserModal" tabindex="-1" role="dialog" aria-labelledby="editUserModal" aria-hidden="true">
-		  <div class="modal-dialog modal-dialog-centered" role="document">
-		    <div class="modal-content">
-		      <div class="modal-header">
-		        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
-		        <button type="button" class="close font-weight-light" data-dismiss="modal" aria-label="Close">
-		          <span aria-hidden="true">&times;</span>
-		        </button>
-		      </div>
-		      <div class="modal-body">
-		        
-		      </div>
-		      <div class="modal-footer">
-		        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-		        <button type="button" class="btn btn-primary">Save changes</button>
-		      </div>
-		    </div>
-		  </div>
-		</div>
+	<?php 
+		// generovanie skriptov pre konkretnu tabulku
+		echo $table->generate_table_scripts();
 
+		?>
 
-     <script>
-
-     	$(document).ready(function(){
-     		/*
-     		*	Function checks all checkboxes on page
-     		*/
-			$("#mytable #checkall").click(function () {
-			        if ($("#mytable #checkall").is(':checked')) {
-			            $("#mytable input[type=checkbox]").each(function () {
-			                $(this).prop("checked", true);
-			            });
-
-			        } else {
-			            $("#mytable input[type=checkbox]").each(function () {
-			                $(this).prop("checked", false);
-			            });
-			        }
-			    });
-			    
-			    $("[data-toggle=tooltip]").tooltip();
-
-			/**
-			 * 	Function changes GET parameter page, to change page of table
-			 */
-			$(".page-link").each(function(){
-				var id = $(this).attr('id');
-				var url = window.location.href;
-				var index = url.indexOf('?');
-				// Remove other params
-				if (index > -1) {
-		            url = url.substr(0,index);
-		        }
-		        url = url + "?page=" + encodeURIComponent(id);
-
-				$(this).attr("href",url);
-			});
-
-		});
-
-		/**
-     	 * 	Function sends ajax to back-end to delete user with id
-     	*/
-     	function delete_user(id,ask=true){
-     		if(ask){
-     			var confirm = window.confirm("Naozaj chcete zmazať usera s ID "+id+" ?");
-     			if(!confirm) return;
-     		}
-     		
-     		var formData={
-     			"action" : "delete",
-     			"user_id" : id 
-     		};
-
-	     	$.ajax({
-	            type: "POST",
-	            url: "/ajax/admin_user_action.php",
-	            data: formData,
-	            dataType: "json",
-	            encode: true,
-	          }).done(function (data) {
-	            if(data.success){
-	              $("#row"+id).hide();
-	            }
-	            else{
-	              var alert = "<div class='alert alert-warning alert-dismissible' role='alert'>" 
-	              + "<a href='#' class='close font-weight-light' data-dismiss='alert' aria-label='close'>&times;</a>"
-	              + data.error + "</div>";
-	            }
-
-	          });
-     	}
-
-     	/**
-     	 * 	Deletes all checked rows.
-     	 *	
-     	*/
-     	function delete_checked(){
-     		var ids = new Array();
-     		$('input[type=checkbox].checkthis').each(function () {
-     			if(this.checked){
-			    	ids.push($(this).val());
-			    }
-			});
-			var confirm = window.confirm("Naozaj chcete zmazať všetky označené riadky ?");
-     		if(!confirm) return;
-
-     		ids.forEach(function(el,index,ids){
-     			delete_user(el,false);
-     		});
-     	}
-
-     </script>
 
    
 	</body>
