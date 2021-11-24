9871e58ce145cd3986cf5ea38ca0db1debc6eb10
Martin Matějka <martin@matejka.xyz>
2021-11-24 13:09:16 +0100

    final touch

--- db_init.sql
+++ db_init.sql
@@ -2,10 +2,10 @@
 -- version 4.9.5deb2
 -- https://www.phpmyadmin.net/
 --
--- Hostiteľ: localhost:3306
--- Čas generovania: So 20.Nov 2021, 13:40
--- Verzia serveru: 10.3.25-MariaDB-0ubuntu0.20.04.1
--- Verzia PHP: 7.4.16
+-- Host: localhost:3306
+-- Generation Time: Nov 24, 2021 at 01:08 PM
+-- Server version: 10.3.25-MariaDB-0ubuntu0.20.04.1
+-- PHP Version: 7.4.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,13 +19,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Databáza: `iis`
+-- Database: `iis`
 --
 
 -- --------------------------------------------------------
 
 --
--- Štruktúra tabuľky pre tabuľku `Conference`
+-- Table structure for table `Conference`
 --
 
 CREATE TABLE `Conference` (
@@ -36,7 +36,7 @@ CREATE TABLE `Conference` (
   `street` varchar(150) COLLATE utf8_slovak_ci NOT NULL,
   `city` varchar(150) COLLATE utf8_slovak_ci NOT NULL,
   `zip` int(11) NOT NULL,
-  `state` text COLLATE utf8_slovak_ci NOT NULL,
+  `state` varchar(150) COLLATE utf8_slovak_ci NOT NULL,
   `time_from` int(11) NOT NULL,
   `time_to` int(11) NOT NULL,
   `price` float NOT NULL,
@@ -47,7 +47,7 @@ CREATE TABLE `Conference` (
 -- --------------------------------------------------------
 
 --
--- Štruktúra tabuľky pre tabuľku `cross_conf_tag`
+-- Table structure for table `cross_conf_tag`
 --
 
 CREATE TABLE `cross_conf_tag` (
@@ -58,7 +58,7 @@ CREATE TABLE `cross_conf_tag` (
 -- --------------------------------------------------------
 
 --
--- Štruktúra tabuľky pre tabuľku `Lecture`
+-- Table structure for table `Lecture`
 --
 
 CREATE TABLE `Lecture` (
@@ -68,16 +68,16 @@ CREATE TABLE `Lecture` (
   `time_from` int(11) DEFAULT NULL,
   `time_to` int(11) DEFAULT NULL,
   `img_url` varchar(300) CHARACTER SET utf8 COLLATE utf8_slovak_ci DEFAULT '/img/lecture_placholder.jpg',
-  `room_id` int(11) DEFAULT NULL,
-  `id_user` int(11) NOT NULL,
   `conference_id` int(11) NOT NULL,
+  `room_id` int(11) DEFAULT NULL,
+  `id_user` int(100) NOT NULL,
   `status` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Štruktúra tabuľky pre tabuľku `Question`
+-- Table structure for table `Question`
 --
 
 CREATE TABLE `Question` (
@@ -91,7 +91,7 @@ CREATE TABLE `Question` (
 -- --------------------------------------------------------
 
 --
--- Štruktúra tabuľky pre tabuľku `Reservation`
+-- Table structure for table `Reservation`
 --
 
 CREATE TABLE `Reservation` (
@@ -113,7 +113,7 @@ CREATE TABLE `Reservation` (
 -- --------------------------------------------------------
 
 --
--- Štruktúra tabuľky pre tabuľku `Room`
+-- Table structure for table `Room`
 --
 
 CREATE TABLE `Room` (
@@ -125,7 +125,7 @@ CREATE TABLE `Room` (
 -- --------------------------------------------------------
 
 --
--- Štruktúra tabuľky pre tabuľku `Schedule`
+-- Table structure for table `Schedule`
 --
 
 CREATE TABLE `Schedule` (
@@ -136,7 +136,7 @@ CREATE TABLE `Schedule` (
 -- --------------------------------------------------------
 
 --
--- Štruktúra tabuľky pre tabuľku `Tag`
+-- Table structure for table `Tag`
 --
 
 CREATE TABLE `Tag` (
@@ -147,7 +147,19 @@ CREATE TABLE `Tag` (
 -- --------------------------------------------------------
 
 --
--- Štruktúra tabuľky pre tabuľku `User`
+-- Table structure for table `Ticket`
+--
+
+CREATE TABLE `Ticket` (
+  `id` int(11) NOT NULL,
+  `reservation_id` int(11) NOT NULL,
+  `hash` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `User`
 --
 
 CREATE TABLE `User` (
@@ -164,25 +176,25 @@ CREATE TABLE `User` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_slovak_ci;
 
 --
--- Kľúče pre exportované tabuľky
+-- Indexes for dumped tables
 --
 
 --
--- Indexy pre tabuľku `Conference`
+-- Indexes for table `Conference`
 --
 ALTER TABLE `Conference`
   ADD PRIMARY KEY (`id`),
   ADD KEY `FK_id_user` (`id_user`);
 
 --
--- Indexy pre tabuľku `cross_conf_tag`
+-- Indexes for table `cross_conf_tag`
 --
 ALTER TABLE `cross_conf_tag`
   ADD PRIMARY KEY (`conference_id`,`tag_id`),
   ADD KEY `FK_Genre` (`tag_id`);
 
 --
--- Indexy pre tabuľku `Lecture`
+-- Indexes for table `Lecture`
 --
 ALTER TABLE `Lecture`
   ADD PRIMARY KEY (`id`),
@@ -191,7 +203,7 @@ ALTER TABLE `Lecture`
   ADD KEY `FK_Lecture_Conference` (`conference_id`);
 
 --
--- Indexy pre tabuľku `Question`
+-- Indexes for table `Question`
 --
 ALTER TABLE `Question`
   ADD PRIMARY KEY (`id`),
@@ -199,7 +211,7 @@ ALTER TABLE `Question`
   ADD KEY `FK_question_lecture` (`lecture_id`);
 
 --
--- Indexy pre tabuľku `Reservation`
+-- Indexes for table `Reservation`
 --
 ALTER TABLE `Reservation`
   ADD PRIMARY KEY (`id`),
@@ -207,128 +219,147 @@ ALTER TABLE `Reservation`
   ADD KEY `user_id` (`user_id`);
 
 --
--- Indexy pre tabuľku `Room`
+-- Indexes for table `Room`
 --
 ALTER TABLE `Room`
   ADD PRIMARY KEY (`id`),
   ADD KEY `FK_Room` (`conference_id`);
 
 --
--- Indexy pre tabuľku `Schedule`
+-- Indexes for table `Schedule`
 --
 ALTER TABLE `Schedule`
   ADD PRIMARY KEY (`id_user`,`id_lecture`),
   ADD KEY `id_lecture` (`id_lecture`);
 
 --
--- Indexy pre tabuľku `Tag`
+-- Indexes for table `Tag`
 --
 ALTER TABLE `Tag`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexy pre tabuľku `User`
+-- Indexes for table `Ticket`
+--
+ALTER TABLE `Ticket`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `reservation_id` (`reservation_id`);
+
+--
+-- Indexes for table `User`
 --
 ALTER TABLE `User`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`);
 
 --
--- AUTO_INCREMENT pre exportované tabuľky
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT pre tabuľku `Conference`
+-- AUTO_INCREMENT for table `Conference`
 --
 ALTER TABLE `Conference`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT pre tabuľku `Lecture`
+-- AUTO_INCREMENT for table `Lecture`
 --
 ALTER TABLE `Lecture`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT pre tabuľku `Question`
+-- AUTO_INCREMENT for table `Question`
 --
 ALTER TABLE `Question`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT pre tabuľku `Reservation`
+-- AUTO_INCREMENT for table `Reservation`
 --
 ALTER TABLE `Reservation`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT pre tabuľku `Room`
+-- AUTO_INCREMENT for table `Room`
 --
 ALTER TABLE `Room`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT pre tabuľku `Tag`
+-- AUTO_INCREMENT for table `Tag`
 --
 ALTER TABLE `Tag`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT pre tabuľku `User`
+-- AUTO_INCREMENT for table `Ticket`
+--
+ALTER TABLE `Ticket`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `User`
 --
 ALTER TABLE `User`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- Obmedzenie pre exportované tabuľky
+-- Constraints for dumped tables
 --
 
 --
--- Obmedzenie pre tabuľku `Conference`
+-- Constraints for table `Conference`
 --
 ALTER TABLE `Conference`
   ADD CONSTRAINT `FK_id_user` FOREIGN KEY (`id_user`) REFERENCES `User` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Obmedzenie pre tabuľku `cross_conf_tag`
+-- Constraints for table `cross_conf_tag`
 --
 ALTER TABLE `cross_conf_tag`
   ADD CONSTRAINT `FK_Conference` FOREIGN KEY (`conference_id`) REFERENCES `Conference` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `FK_Genre` FOREIGN KEY (`tag_id`) REFERENCES `Tag` (`id`) ON DELETE CASCADE;
 
 --
--- Obmedzenie pre tabuľku `Lecture`
+-- Constraints for table `Lecture`
 --
 ALTER TABLE `Lecture`
   ADD CONSTRAINT `FK_Lecture_Conference` FOREIGN KEY (`conference_id`) REFERENCES `Conference` (`id`),
   ADD CONSTRAINT `FK_User` FOREIGN KEY (`id_user`) REFERENCES `User` (`id`);
 
 --
--- Obmedzenie pre tabuľku `Question`
+-- Constraints for table `Question`
 --
 ALTER TABLE `Question`
   ADD CONSTRAINT `FK_question_lecture` FOREIGN KEY (`lecture_id`) REFERENCES `Lecture` (`id`),
   ADD CONSTRAINT `Question_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Obmedzenie pre tabuľku `Reservation`
+-- Constraints for table `Reservation`
 --
 ALTER TABLE `Reservation`
   ADD CONSTRAINT `FK_Reservation` FOREIGN KEY (`conference_id`) REFERENCES `Conference` (`id`),
   ADD CONSTRAINT `Reservation_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Obmedzenie pre tabuľku `Room`
+-- Constraints for table `Room`
 --
 ALTER TABLE `Room`
   ADD CONSTRAINT `FK_Room` FOREIGN KEY (`conference_id`) REFERENCES `Conference` (`id`);
 
 --
--- Obmedzenie pre tabuľku `Schedule`
+-- Constraints for table `Schedule`
 --
 ALTER TABLE `Schedule`
   ADD CONSTRAINT `Schedule_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `User` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `Schedule_ibfk_2` FOREIGN KEY (`id_lecture`) REFERENCES `Lecture` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `Ticket`
+--
+ALTER TABLE `Ticket`
+  ADD CONSTRAINT `Ticket_ibfk_1` FOREIGN KEY (`reservation_id`) REFERENCES `Reservation` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
